06e3c854b21b62f4d75eac0cc1648609fddd2ed6
YasoKarim <yasoelshafei@gmail.com>
2022-12-28 10:26:28 +0200

    Contact_us form validation and Slideshow

--- Javascript_files/Javascript.js
+++ Javascript_files/Javascript.js
@@ -113,6 +113,143 @@ if(email == "")
   return valid;
 }
 
+var c =0;
+var images= [];
+var time = 3000;
+images[0] = '1.jpg'; 
+images[1] = '2.jpg'; 
+images[2] = '3.jpg'; 
+
+function next()
+{
+document.getElementById("slideshow").src = images[c];
+if(c < images.length -1)
+{
+  c++;
+}
+else{
+c = 0;}
+
+setTimeout("next()", time);
+
+}
+window.onload =next;
+
+
+
+  /*	// contain images in an array
+    var image = ['1.jpg','2.jpg', '3.jpg'];
+
+    var i = 0;
+
+
+    // function for next slide 
+
+    function nextImage(){
+      document.getElementById("slideshow").src = image[i];
+    	if ( i < image.length - 1) {
+    		i= i+1;
+    	}else{
+    		i = 0;
+    	}
+     slider_content.innerHTML = "<img src="+image[i-1]+".jpg>";
+    setTimeout("nextImage()",2000);
+    }
+window.onload = nextImage;
+
+    // function for prew slide
+
+   /* function prewImage(){
+
+    	if (i<image.length+1 && i>1) {
+    		i= i-1;
+    	}else{
+    		i = image.length;
+    	}
+    	  slider_content.innerHTML = "<img src="+image[i-1]+".jpg>";
+
+      
+
+    }
+    setInterval(nextImage , 4000);
+  */
+  // script for auto image slider
+
+function validate()
+{
+  var valid = false;
+  var formlabels = document.getElementsByTagName("label");
+  var firstname = document.contactusform.firstname.value;
+  var lastname = document.contactusform.lastname.value; 
+  var message = document.contactusform.message.value;
+  var email = document.contactusform.useremail.value; 
+   
+  if(firstname == "") 
+  {
+  formlabels[0].innerHTML = "First Name: * [Required]";
+  formlabels[0].style.color = "red";
+  valid = false;
+  }
+  
+  else if(!isNaN(firstname))
+  {
+    formlabels[0].innerHTML = "First Name: * [Text only]";
+    formlabels[0].style.color = "red";
+    valid = false;
+  }
+  
+  else
+  {
+  formlabels[0].innerHTML = "First Name: *";
+  formlabels[0].style.color = "black";
+  valid = (valid) ? true: false;
+  }
+
+  if(lastname == "" )
+  {
+    formlabels[1].innerHTML = "Last Name: * [Required]";
+    formlabels[1].style.color = "red";
+    valid =false;
+  }
+  else if(!isNaN(lastname)) 
+  {
+    formlabels[1].innerHTML = "Last Name: * [Text only]";
+    formlabels[1].style.color = "red";
+    valid = false;
+  }
+  else
+  {
+  formlabels[1].innerHTML = "Last Name: *";
+  formlabels[1].style.color = "black";
+  valid = (valid) ? true: false;
+  }
+var emailregex =/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;  
+if(email == "")
+  {
+    formlabels[2].innerHTML = "Email : * [Required]";
+    formlabels[2].style.color = "red";
+    valid = false;
+  }
+  else if(emailregex.test(email))
+  {
+    formlabels[2].innerHTML = "Email : *";
+    formlabels[2].style.color = "black";
+    valid = (valid) ? true: false;
+  }
+  else  
+  {  
+    formlabels[2].innerHTML = "Email : * [Incorrect email]";
+    formlabels[2].style.color = "red";
+    valid = false;
+  }
+  if(message = "")
+  {
+  formlabels[3].innerHTML = "Message: * [Required]";
+  formlabels[3].style.color = "red";
+  valid = false;
+  }
+  return valid;
+}
 //var at = email.indexOf("@");
 //var dot = email.indexOf(".");
 //var validRegex = /^([A-Za-z0-9_\-\.])+\@([A-Za-z0-9_\-\.])+\.([A-Za-z] (2,4))$/;
